9a820d4c15f0af82d90883ca3c226eb089f78f73
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-03-20 21:36:44 -0300

    feat/ added-routes-and-imported-on-server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
+import cakesRouter from './routes/cakes.routes.js';
+import ordersRouter from './routes/orders.routes.js';
+import clientsRouter from './routes/clients.routes.js';
 dotenv.config()
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
+
+app.use([cakesRouter, ordersRouter, clientsRouter])
 
 app.listen(process.env.DATABASE_URL || 5000, ()=>{
     console.log('Server running')
