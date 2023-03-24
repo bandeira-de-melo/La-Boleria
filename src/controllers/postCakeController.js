b2036e0016a4dff24f365263685a4f278262d57e
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-03-23 22:15:51 -0300

    feat/add postCakeController

--- src/controllers/postCakeController.js
+++ src/controllers/postCakeController.js
@@ -1,8 +1,12 @@
+import { db } from "../config/database.js";
+
 async function postCakeController(req, res) {
     const {name, price, image, description} = res.locals.data;
     try {
-        await db.query()
+        const isCake = await db.query('SELECT * FROM cakes WHERE name=$1',[name])
+        if(isCake) return res.status(409)
+        res.send(name)
     } catch (error) {
-        
+        res.status(500).send(error)
     }
 }
